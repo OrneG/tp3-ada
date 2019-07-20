45cf5ffe0b3a90e86a700fe9eb07ced0d50ea9b9
Ornella Giaccarini <ornegiaccarini@gmail.com>
2019-07-20 15:11:13 -0300

    secciones agregadas

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,23 @@
 const apiKey = '77c9a071477a6f097501ea60b348aab8';
 const poster = document.getElementById('movie-poster');
 const title = document.getElementById('movie-title');
-const movie = document.getElementById('movies-body');
-const movieModel = movie.children[0];
+const movieSection = document.getElementsByClassName('movies-section');
+const popular = document.getElementById('popular-section');
+const topRated = document.getElementById('top-rated-section');
+const upcoming = document.getElementById('upcoming-section');
+const nowPlaying = document.getElementById('now-playing-section');
 
-const updatePopular = () => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+const getMovies = (url, movie) => {
+    fetch(url)
         .then(response => response.json())
         .then(data => {
             if (!data.results.length) {
                 return;
             }
             console.log(data);
+            const movieModel = movie.children[0];
 
-            const fiveMovies = data.results.slice(0,5);
+            const fiveMovies = data.results.slice(0, 5);
             movie.innerHTML = '';
 
             for (let i = 0; i < fiveMovies.length; i++) {
@@ -27,4 +31,26 @@ const updatePopular = () => {
         .catch();
 }
 
+const updatePopular = () => {
+    getMovies(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`, popular)
+}
+
 updatePopular();
+
+const updateTopRated = () => {
+    getMovies(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`, topRated)
+}
+
+updateTopRated();
+
+const updateUpcomingMovies = () => {
+    getMovies(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`, upcoming)
+}
+
+updateUpcomingMovies();
+
+const updateNowPlaying = () => {
+    getMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`, nowPlaying)
+}
+
+updateNowPlaying();
